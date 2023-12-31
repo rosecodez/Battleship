e33b79ece76a3db4f44bf5247e510f0d5e29993b
Rose <rosebeats09@gmail.com>
2023-12-30 19:15:15 -0500

    Create gameboard class constructor

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,3 +1,28 @@
 class Gameboard {
-    constructor()
+  constructor(row, col) {
+    const shipsSunkArray = [];
+    const grid = new Array(row).fill(new Array(col).fill(0));
+    return grid;
+  }
+
+  // place ship at specific coordinates bt calling ship constructor
+  placeShip(row, col) {
+
+  }
+
+  receiveAttack(row, col) {
+
+  }
+
+  allSunk() {
+    if (shipsSunkArray.length !== 0) {
+      console.log('all ships were sunk');
+      return true;
+    }
+    console.log('not all ships were sunk');
+    return false;
+  }
 }
+module.exports = Gameboard;
+const testGameboard = new Gameboard(10, 10);
+console.log(testGameboard);
