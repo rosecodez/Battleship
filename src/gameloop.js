514b801d3f400228a7d4cfc1c930845b89fb29b4
Rose <rosebeats09@gmail.com>
2024-01-07 18:05:41 -0500

    Set instructions and next steps for game loop

---
+++ src/gameloop.js
@@ -0,0 +1,15 @@
+// 1.
+// set up a new game
+// create players and gameboards
+// populate each gameboard with predetermined coordinates
+// implement a system for allowing to place ships *later*
+
+// 2.
+// game loop should step through the game turn by turn
+// using only methods from other objects
+// if at any point i am tempted to write a new function inside the game loop
+// step back and figure out which class or module that function should belong to
+
+// 3.
+// create conditions so that the game ends once one player's ships have all been sunk
+// this function is appropiate for the game modules
