8b4177bb1d4967bf065ae5d5e6dd1d237f7361b4
Rose <rosebeats09@gmail.com>
2024-01-12 20:44:26 -0500

    Add webpack

--- src/gameloop.js
+++ src/gameloop.js
@@ -13,4 +13,17 @@
 // 3.
 // create conditions so that the game ends once one player's ships have all been sunk
 // this function is appropriate for the game modules
+import _ from 'lodash';
 import './style.css';
+
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  return element;
+}
+
+document.body.appendChild(component());
